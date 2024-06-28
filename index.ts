379f260bba4b0a3977dfa777f9500260b2814b1d
QPM <admin@QuantumPinnacleMatrix.tech>
2024-06-28 16:23:37 +0300

    setup prisma and express

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello Man!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
