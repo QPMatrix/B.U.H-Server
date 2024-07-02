b451b6faef404611dfe96440233ac6546c82662e
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-02 11:35:35 +0300

    setup prisma

--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
+import prisma from "@lib/prisma";
 import express from "express";
 const app = express();
-
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+const connect = async () => {
+  try {
+    await prisma
+      .$connect()
+      .then(() => {
+        console.log("Database connected");
+      })
+      .catch((error) => {
+        console.log("Database connection failed", error);
+      });
+  } catch (error) {
+    throw error;
+  }
+};
+app.listen(PORT, () => {
+  connect();
   console.log("Server is running on port 8080");
 });
